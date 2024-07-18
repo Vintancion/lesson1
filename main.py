95499e81fafe123637f7ebe3bd576d0a71d6bed6
Vintancion <Vitalikarshinov1122@gmail.com>
2024-07-18 22:58:14 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+#"1st program"
+print (9 ** 0.5 * 5)
+
+#"2nd program"
+print (9.99 > 9.98 and 1000 != 1000.1)
+
+#"3rd program"
+example = (1234 % 1000 and 234 // 10) + (5678 % 1000 and 678 // 10) 
+print (example)
+#"4th program"
+example = (int(13.42 and 42.13) and (13 == 13) or (42 == 42))
+print (example)
